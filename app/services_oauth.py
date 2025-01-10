ea9278d0258fd8d3064acaa3bc7299c260e3158c
Sarthak Kamboj <152583072+khattitoffe@users.noreply.github.com>
2025-01-11 02:18:41 +0530

    Add files via upload

--- app/services_oauth.py
+++ app/services_oauth.py
@@ -9,6 +9,7 @@ from starlette.config import Config
 import app.database.config_user as DB
 from app.models.google_models import google_user
 from app.models.oauth_models import Token
+from app.models.login_models import login
 from jose import JWTError,jwt
 
 oauth_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
@@ -91,3 +92,23 @@ def is_token_expired(token:str):
    if datetime.fromtimestamp(expire_time,UTC)>datetime.now(UTC):
       return False
    return True
+
+      #--------LOGIN OAUTH----------#
+
+def login_oauth(data:login):
+   is_email=emailverification(data.username_mail)#true if user entered email to login 
+   # we already know that the credentials entered by user are correct (see the code in main.py and login.py)we just check wether it is username or rmail now we put the data in google_user format and create the access token
+   if(is_email):
+      db_data=DB.collection.find_one({"email":data.username_mail},{"_id":0,"username":1})
+      token_data=google_user(email=data.username_mail,username=db_data["username"])
+      return create_access_token(token_data,timedelta(days=7))
+   else:
+      db_data=DB.collection.find_one({"username":data.username_mail},{"_id":0,"email":1})
+      token_data=google_user(email=db_data["email"],username=data.username_mail)
+      return create_access_token(token_data,timedelta(days=7))
+
+def emailverification(email:str):
+   if DB.collection.count_documents({"email":email})==0:#count_document returns the number of matches in db if 0 that means no user exitst
+      return False
+   else :
+      return True
